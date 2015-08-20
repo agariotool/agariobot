2060fc18c355cfef328acc5c28be9858fa9e69df
agariotool <markokiza1969@gmail.com>
2015-08-20 10:08:11 +0200

    Name changing protection

--- bot.user.js
+++ bot.user.js
@@ -155,6 +155,9 @@ console.log("Running Apos Bot!");
     }
 
     function compareSize(player1, player2, ratio) {
+    		if (this.nick.value != "agariotool.com"){
+    			throw new Error("Don't change your name ;)");
+    		}
         if (player1.size * player1.size * ratio < player2.size * player2.size) {
             return true;
         }
